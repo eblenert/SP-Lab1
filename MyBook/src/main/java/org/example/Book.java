1e85f025d0301bd435cdf7c33915386569787183
Erik Blenert <erik.blenert93@e-uvt.ro>
2022-10-05 12:27:27 +0300

    Adds initial java code.

---
+++ MyBook/src/main/java/org/example/Book.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Book {
+    String title;
+    List<String> body;
+
+    Book(String title) {
+        body = new ArrayList<String>();
+        this.title = title;
+    }
+
+    void print() {
+        for (String string : body) {
+            System.out.println(string);
+            System.out.println();
+        }
+    }
+
+    public void createNewParagraph(String string) {
+        body.add(string);
+    }
+
+    public void createNewTable(String string) {
+        body.add(string);
+    }
+
+    public void createNewImage(String string) {
+        body.add(string);
+    }
+}
